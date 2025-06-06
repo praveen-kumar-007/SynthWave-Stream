96240d78ac59f1e544b230dce74beb72c53e372d
Praveen Kumar <praveen.pr105@gmail.com>
2025-06-06 16:11:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- CONFIGURATION ---
 // !!! IMPORTANT: Replace with your actual YouTube Data API v3 Key !!!
 // !!! KEEP THIS SECRET IF DEPLOYING PUBLICLY (use a backend instead) !!!
-const API_KEY = 'YOUR YOUTUBE V3 API KEY';
+const API_KEY = 'AIzaSyDU2q7BubgeOjfBfQm3x_0u3CZAkUWCN54';
 const MAX_RESULTS = 15; // Number of search results to fetch
 
 // --- DOM ELEMENTS ---
